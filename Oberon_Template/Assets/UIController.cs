363d08d0c588fec7f76e339d728ee85bf353f0e4
jmhoff1818 <55460773+jmhoff1818@users.noreply.github.com>
2022-10-23 15:17:57 -0400

    started on menu

---
+++ Oberon_Template/Assets/UIController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+
+public class UIController : MonoBehaviour
+{
+
+    public Button startButton;
+    public Button messageButton;
+    public Label messageText;
+
+    [Header("ScenePlacement")]
+    public Scene loadedScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+
+        startButton = root.Q<Button>("start-button");
+        messageButton = root.Q<Button>("message-button");
+        messageText = root.Q<Label>("message-text");
+
+        startButton.clicked += StartButtonPressed;
+        messageButton.clicked += MessageButtonPressed;
+
+
+    }
+
+    // Update is called once per frame
+    void StartButtonPressed()
+    {
+        SceneManager.LoadScene("Main");
+
+    }
+
+    void MessageButtonPressed()
+    {
+        messageText.text = "Testing this here";
+        messageText.style.display = DisplayStyle.Flex;
+    }
+}
