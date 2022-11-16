ee75d7bea17bb7a1b2647840a5e66204e8bcdf5d
Owallace4 <wallaco2@miamioh.edu>
2022-11-16 13:32:23 -0500

    UI Update 2
    Centered buttons, Added scene switching for game start.

--- Oberon_Template/Assets/UI/OmarUIFolder/MenuUI.cs
+++ Oberon_Template/Assets/UI/OmarUIFolder/MenuUI.cs
@@ -2,14 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
 public class MenuUI : MonoBehaviour
 {
     private void Awake()
     {
         VisualElement root = GetComponent<UIDocument>().rootVisualElement; 
-        root.Q<Button>("StartGame").clicked += () => Debug.Log("Start Button Clicked");
+        root.Q<Button>("StartGame").clicked += () => changeScene(1);
         root.Q<Button>("Options").clicked += () => Debug.Log("Options Clacked");
-        root.Q<Button>("QuitGame").clicked += () => Debug.Log("Start Button Clucked");
+        root.Q<Button>("QuitGame").clicked += () => Application.Quit();
+    }
+
+    public void changeScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
     }
 }
