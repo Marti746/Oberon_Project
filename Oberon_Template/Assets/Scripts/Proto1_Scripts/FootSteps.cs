e530497f6fa4aa374a7df78bb6c5d1402a03070a
David Martin <69823918+Marti746@users.noreply.github.com>
2022-11-30 13:15:26 -0500

    new things for controllers working with escape

--- Oberon_Template/Assets/Scripts/Proto1_Scripts/FootSteps.cs
+++ Oberon_Template/Assets/Scripts/Proto1_Scripts/FootSteps.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Rewired;
 
 public class FootSteps : MonoBehaviour
 {
@@ -11,14 +12,18 @@ public class FootSteps : MonoBehaviour
     public AudioSource crouchSlideSound;
     private PlayerMovement pm;
 
+    [SerializeField] private int playerID = 0;
+    [SerializeField] private Player player;
+
     void Start()
     {
         pm = GetComponent<PlayerMovement>();
+        player = ReInput.players.GetPlayer(playerID);
     }
 
     void Update()
     {
-        if(Input.GetKey(KeyCode.Escape)) {
+        if(player.GetButtonDown("BackMenu")) {
             SceneManager.LoadScene("Main Menu");
         }
 
