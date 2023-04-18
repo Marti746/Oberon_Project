2ba2eb03e916507eadddf2fd2c91a78a85e5b50b
Christopher Vlaha <chris@vlaha.com>
2023-04-18 10:33:41 -0400

    revert

--- Oberon_Template/Assets/Art/Shaders/Includes/Lighting.hlsl
+++ Oberon_Template/Assets/Art/Shaders/Includes/Lighting.hlsl
@@ -1,26 +1,15 @@
 #ifndef CUSTOM_LIGHTING_INCLUDED
 #define CUSTOM_LIGHTING_INCLUDED
-#pragma multi_compile _MAIN_LIGHT_CALCULATE_SHADOWS _MAIN_LIGHT_SHADOWS_CASCADE _SHADOWS_SOFT
 
-void CalculateMainLight_float(float3 WorldPos, out float3 Direction, out float3 Color, out half DistanceAtten, out half ShadowAtten)
+void CalculateMainLight_float(float3 WorldPos, out float3 Direction, out float3 Color)
 {
 #ifdef SHADERGRAPH_PREVIEW
-	   Direction = half3(0.5, 0.5, 0);
+	   Direction = float3(0.5, 0.5, 0);
 	   Color = 1;
-	   DistanceAtten = 1;
-	   ShadowAtten = 1;
 #else
-	#if SHADOWS_SCREEN
-		half4 clipPos = TransformWorldToHClip(WorldPos);
-		half4 shadowCoord = ComputeScreenPos(clipPos);
-	#else
-		half4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
-	#endif
-	Light mainLight = GetMainLight(shadowCoord);
+	Light mainLight = GetMainLight(0);
 	Direction = mainLight.direction;
 	Color = mainLight.color;
-	DistanceAtten = mainLight.distanceAttenuation;
-	ShadowAtten = mainLight.shadowAttenuation;
 #endif  
 }
 #endif
